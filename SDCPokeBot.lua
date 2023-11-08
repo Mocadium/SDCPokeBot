ab5d8367d9c9673f11150ce970a6495fd6763692
Mocadium <mocadium@gmail.com>
2023-11-08 22:38:36 +1300

    Calculating Shiny Value for Starter
    Calculating Shiny Value for Starter (1st Party Pokemon

--- SDCPokeBot.lua
+++ SDCPokeBot.lua
@@ -1,17 +1,34 @@
---RAM READING FUNCTIONS--
+--STATES--
+function StateStarter()
+    PID = emu:read32(0x02024284);
+    buffer:print("Pokemon ID: " .. PID)
+    buffer:moveCursor(0, 1)
+    GetShinyValue(PID)
+    --buffer:print("Shiny Value: " .. GetShinyValue(PID))
+end
 
+--MISC FUNCTIONS--
 function GetPlayerData()
-
     address = emu:read32(0x03005008)
 
     player = {}
     player.x = emu:read16(address)
     player.y = emu:read16(address+2)
 
-    buffer:moveCursor(0, 0)
     buffer:print("Player X: " .. player.x .. ", Y: " .. player.y)
     buffer:moveCursor(0, 1)
     buffer:print("TID: " .. tostring(TID).. ", SID: " .. tostring(SID))
+end
+
+function GetShinyValue(PID)
+    p1 = PID >> 16
+    p2 = PID & 0xFFFF
+    SV = TID ~ SID ~ p1 ~ p2
+
+    buffer:print("P1: " .. p1 .. ", P2: " .. p2)
+    buffer:moveCursor(0, 2)
+    buffer:print("Shiny Value:" .. SV)
+    return
 end
 
 --CREATE FUNCTION--
@@ -20,7 +37,7 @@ function Create()
     buffer = console:createBuffer("Buffer")
 
     --CONSTS--
-    state = GetPlayerData;
+    state = StateStarter;
 
     --IDs--
     IDaddress = emu:read32(0x0300500C)
@@ -37,6 +54,7 @@ function Step()
 		return
 	end
     buffer:clear()
+    buffer:moveCursor(0, 0)
 
     --STATE--
     if state ~= nil then
